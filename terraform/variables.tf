77b319df2c1a4cff6ecdcd252d83c528df73792e
divaychauhan <divaypratap5113@gmail.com>
2026-06-20 11:05:44 +0000

    Add Terraform and deployment files

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "service_name" {
+  description = "Cloud Run Service Name"
+  type        = string
+  default     = "demo-app"
+}
